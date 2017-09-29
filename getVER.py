5d80dae52db89d2cdcc321c1176bb44cf5b4d79f
tgzt <32170685+tgzt@users.noreply.github.com>
2017-09-29 12:06:43 -0400

    add header info

--- getVER.py
+++ getVER.py
@@ -5,10 +5,11 @@ import requests
 usr = "ers"
 pwd = "ersM0"
 ise = "ise.secure-x.local"
+headers = {"Content-Type" : "application/vnd.com.cisco.ise.identity.internaluser.1.0+xml"}
 
 url= "https://%s:9060/ers/config/internaluser/versioninfo" % ise
 
-r = requests.get(url,verify=False,auth=(usr,pwd))
+r = requests.get(url,verify=False,auth=(usr,pwd),headers=headers)
 #
 print "CODE:",r.status_code
 print "HEADERS:",r.headers
