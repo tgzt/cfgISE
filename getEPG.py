27b9437746057595f7f74fbaa4ebd2238b529374
tgzt <32170685+tgzt@users.noreply.github.com>
2017-09-29 11:10:02 -0400

    generalized call

--- getEPG.py
+++ getEPG.py
@@ -1,7 +1,14 @@
 #! /usr/bin/env python2.7
 # get Endpoint Groups
+#
+usr = "admin"
+pwd = "Ci5coAdmin"
+ise = "ise.secure-x.local"
 
-url= "https://%s:%s@ise-secure-x.local:9060/ers/config.endpoint"
+url= "https://%s:%s@ise-secure-x.local:9060/ers/config.endpoint" % (usr,pwd,ise)
 
 r = requests.get(url,auth=(usr,pwd))
-r.status_code
+#
+print r.status_code
+print r.headers
+print r.content
