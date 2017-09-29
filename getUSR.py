e807003039636855dffeb7a665ddd96ce37fe40d
thomas.graham <thomas.graham@zolontech.com>
2017-09-29 18:17:09 +0100

    LIST USERS - IT WORKS

---
+++ getUSR.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python2.7
+# get All users
+import requests
+#
+usr = "ers"
+pwd = "ersM0"
+ise = "ise.secure-x.local"
+headers = {"Accept" : "application/vnd.com.cisco.ise.identity.internaluser.1.0+xml"}
+
+url= "https://%s:9060/ers/config/internaluser" % ise
+
+r = requests.get(url,verify=False,auth=(usr,pwd),headers=headers)
+#
+print "CODE:",r.status_code
+print "HEADERS:",r.headers
+print "CONTENT:",r.content
