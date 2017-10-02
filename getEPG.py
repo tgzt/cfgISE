a3ff7827f7c1603d9e245c1b6ce6b31e35898b90
root <root@localhost.localdomain>
2017-10-02 16:24:00 +0100

    working on epg

--- getEPG.py
+++ getEPG.py
@@ -1,13 +1,12 @@
-#! /usr/bin/env python2.7
 # get Endpoint Groups
 import requests
 #
 usr = "ers"
 pwd = "ersM0"
 ise = "ise.secure-x.local"
-headers = {"Accept" : "application/vnd.com.cisco.ise.identity.internaluser.1.0+xml"}
+headers = {"Accept" : "application/vnd.com.cisco.ise.identity.endpointgroup.1.0+xml"}
 
-url= "https://%s:9060/ers/config.endpoint" % ise
+url= "https://%s:9060/ers/config/endpointgroup" % ise
 
 r = requests.get(url,verify=False,auth=(usr,pwd))
 #
